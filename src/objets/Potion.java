b0e30680d749fe5e3d038b73c81b8b6d15935e3b
TJeanm <theo.jeanmart@univ-tlse3.fr>
2025-02-06 12:01:54 +0100

    corrections

--- src/objets/Potion.java
+++ src/objets/Potion.java
@@ -1,7 +1,11 @@
 package objets;
 
+import java.util.Random;
+
 public class Potion {
     private final int puissance;
+    private Random random;
+    private int dose;
 
     public Potion(int puissance) {
         this.puissance = puissance;
@@ -10,5 +14,23 @@ public class Potion {
     public int getPuissance() {
         return puissance;
     }
+    
+    
+    
+    public int getDose() {
+		return dose;
+	}e
+
+	public void fabriquerPotion(int quantite) {
+		if (quantite + nombrePotions > stockPotions.length) {
+			augmenterTailleStock(quantite + nombrePotions - stockPotions.length);
+		}
+		int puissance = 2 + random.nextInt(5);
+		for (int i = 0; i < quantite; i++) {
+
+			stockPotions[nombrePotions++] = new Potion(puissance);
+		}
+		this.parler("J'ai concocté " + quantite + " potions de puissance " + puissance);
+	}
 
 }
